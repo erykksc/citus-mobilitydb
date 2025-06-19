b7c609b5d96456a16ef9b131b3eb1c6d0178a039
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-06-19 22:13:51 +0200

    chore: add flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "An empty flake template that you can adapt to your own environment";
+  description = "An flake for the environment of a Nix-based development machine";
 
   # Flake inputs
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
@@ -19,26 +19,64 @@
       forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import inputs.nixpkgs { inherit system; };
       });
+      /*
+       * Change this value ({major}.{min}) to
+       * update the Python virtual-environment
+       * version. When you do this, make sure
+       * to delete the `.venv` directory to
+       * have the hook rebuild it for the new
+       * version, since it won't overwrite an
+       * existing one. After this, reload the
+       * development shell to rebuild it.
+       * You'll see a warning asking you to
+       * do this when version mismatches are
+       * present. For safety, removal should
+       * be a manual step, even if trivial.
+       */
+      pythonVersion = "3.13";
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          # Add any you need here
-          packages = with pkgs; [
-            kubectl
-            postgresql_17
-          ];
+      devShells = forEachSupportedSystem ({ pkgs }:
+        let
+          concatMajorMinor = v:
+            pkgs.lib.pipe v [
+              pkgs.lib.versions.splitVersion
+              (pkgs.lib.sublist 0 2)
+              pkgs.lib.concatStrings
+            ];
 
-          # Set any environment variables for your dev shell
-          env = {
-            PGPASSWORD="postgres";
+          python = pkgs."python${concatMajorMinor pythonVersion}";
+        in
+        {
+          default = pkgs.mkShell {
+            venvDir = ".venv";
+
+            postShellHook = ''
+              venvVersionWarn() {
+              	local venvVersion
+              	venvVersion="$("$venvDir/bin/python" -c 'import platform; print(platform.python_version())')"
+
+              	[[ "$venvVersion" == "${python.version}" ]] && return
+
+              	cat <<EOF
+              Warning: Python version mismatch: [$venvVersion (venv)] != [${python.version}]
+                       Delete '$venvDir' and reload to rebuild for version ${python.version}
+              EOF
+              }
+
+              venvVersionWarn
+            '';
+
+			packages = [
+							pkgs.nixfmt-rfc-style
+              python.pkgs.venvShellHook
+              python.pkgs.pip
+
+            pkgs.kubectl
+            pkgs.postgresql_17
+
+							];
           };
-
-          # Add any shell logic you want executed any time the environment is activated
-          shellHook = ''
-          '';
-        };
-      });
-    };
+        });
+	};
 }
